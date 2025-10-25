44f4696ec6295b0832663c7024dcd5088a62b279
Rifat770-coder <minhajulislamrifat43@gmail.com>
2025-10-25 22:09:50 +0600

    update2

--- app/Legacy/page.tsx
+++ app/Legacy/page.tsx
@@ -19,7 +19,7 @@ export default function LegacyPage() {
     },
     {
       id: 2,
-      image: "/486664461_1181399783676787_2142824188662298536_n.jpg", // Replace with your actual image path
+      image: "/20251018_003312.jpg", // Replace with your actual image path
       title: "Why is Dig The Data?",
       description: [
         "“Dig The Data” makes learning engaging and interactive by transforming traditional education into a fun, game-based experience. It strengthens logical and analytical thinking while encouraging teamwork and communication as teams collaborate and strategize to solve challenges. "
