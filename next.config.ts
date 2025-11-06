657cb76ade1a9703c5bc9958c322eeb1f897f41a
Rifat770-coder <minhajulislamrifat43@gmail.com>
2025-11-06 09:43:23 +0600

    update25

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'digthedata.live',
+          },
+        ],
+        destination: 'https://www.digthedata.live/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
